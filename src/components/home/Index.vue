aa34533769e0e3819b672da55cfcad13052928bf
czcll49 <xiaobin.chen@maikeos.com>
2020-07-03 19:13:18 +0800

    cxb

--- src/components/home/Index.vue
+++ src/components/home/Index.vue
@@ -1,6 +1,18 @@
-<template>
+<template xmlns:v-slot="http://www.w3.org/1999/XSL/Transform">
     <div>
         <p>{{ msg }}</p>
+        <el-tabs type="border-card">
+            <el-tab-pane>
+                <template slot="label">
+                    <i class="el-icon-date"></i>
+                    <span>我的行程</span>
+                </template>
+                <span>我的行程</span>
+            </el-tab-pane>
+            <el-tab-pane label="消息中心">消息中心</el-tab-pane>
+            <el-tab-pane label="角色管理">角色管理</el-tab-pane>
+            <el-tab-pane label="定时任务补偿">定时任务补偿</el-tab-pane>
+        </el-tabs>
         <el-calendar v-model="value"></el-calendar>
     </div>
 </template>
